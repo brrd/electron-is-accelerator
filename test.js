9027262c32b4ee2341a97d313a62f4be03903291
Andrea Parodi <andrea@parro.it>
2016-11-17 20:04:45 +0100

    Add key codes test

--- test.js
+++ test.js
@@ -1,9 +1,47 @@
 import test from 'ava';
 import isAccelerator from '.';
 
-test('typo', t => t.false(isAccelerator('CommandOrContrl+F9')));
-test('multiple modifier', t => t.true(isAccelerator('CommandOrControl+Shift+Z')));
-test('multiple keys', t => t.false(isAccelerator('A+Z')));
+const keys = [
+    '0',
+    '1',
+    '9',
+    'A',
+    'G',
+    'Z',
+    'F1',
+    'F5',
+    'F24',
+    '~',
+    '!',
+    '@',
+    '#',
+    'Plus',
+    'Space',
+    'Tab',
+    'Backspace',
+    'Delete',
+    'Insert',
+    'Return',
+    'Enter',
+    'Up',
+    'Down',
+    'Left',
+    'Right',
+    'Home',
+    'End',
+    'PageUp',
+    'PageDown',
+    'Escape',
+    'Esc',
+    'VolumeUp',
+    'VolumeDown',
+    'VolumeMute',
+    'MediaNextTrack',
+    'MediaPreviousTrack',
+    'MediaStop',
+    'MediaPlayPause',
+    'PrintScreen'
+];
 
 const modifiers = [
     'Command',
@@ -19,6 +57,10 @@ const modifiers = [
     'Super'
 ];
 
+test('multiple modifier', t => t.true(isAccelerator('CommandOrControl+Shift+Z')));
+test('multiple keys', t => t.false(isAccelerator('A+Z')));
+
+// tests to check each modifiers
 modifiers.forEach(mod => test(
     mod + ' modifier',
     t => t.true(
@@ -26,4 +68,21 @@ modifiers.forEach(mod => test(
     )
 ));
 
+// tests to check all keys
+keys.forEach(key => test(
+    key + ' key',
+    t => t.true(
+        isAccelerator(key)
+    )
+));
 
+
+// tests to check every combination of modifier and key
+modifiers.forEach(mod =>
+    keys.forEach(key => test(
+        `${mod}+${key} sequence`,
+        t => t.true(
+            isAccelerator(`${mod}+${key}`)
+        )
+    ))
+);
